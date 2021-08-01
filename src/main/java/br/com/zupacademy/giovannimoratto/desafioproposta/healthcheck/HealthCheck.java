ef14de5baf256a1d4c705181b5517163f181ae4d
Giovanni Moratto <morattotrabalho@gmail.com>
2021-07-31 21:07:10 -0300

    refactoring health check

---
+++ src/main/java/br/com/zupacademy/giovannimoratto/desafioproposta/healthcheck/HealthCheck.java
@@ -0,0 +1,28 @@
+package br.com.zupacademy.giovannimoratto.desafioproposta.healthcheck;
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.boot.actuate.health.Status;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Author giovanni.moratto
+ */
+
+@Component
+public class HealthCheck implements HealthIndicator {
+
+    @Override
+    public Health health() {
+        Map <String, Object> details = new HashMap <>();
+        details.put("versão", "1.0.0");
+        details.put("descrição", "Heath projeto");
+        details.put("endereço", "127.0.0.1");
+
+        return Health.status(Status.UP).withDetails(details).build();
+    }
+
+}
